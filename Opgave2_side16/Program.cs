cf758e580b65582ff7309364dee193f991176349
Alexander Roikier Møller <alex6308@elev.tec.dk>
2018-08-15 12:19:45 +0200

    <Færdigt resultat der blev tilføjet at den har 2 decimaler med>

---
+++ Opgave2_side16/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgave2_side16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input "decimaltal"
+            Console.WriteLine("Indtast decimaltal");
+            decimal decimaltal = decimal.Parse(Console.ReadLine());
+
+            //Input bliver rundet op til et hel tal med 2 decimaler
+            decimaltal = Math.Round(decimaltal,2);
+           
+            //Udskrivning af "resultat" + tekst
+            Console.WriteLine(decimaltal + " Tallet bliver rundet op til og med 2 decimaler");
+
+            //Slut
+            Console.ReadKey();
+        }
+    }
+}
